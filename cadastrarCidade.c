06fe00511cbf0bfa1817687a76d09f8745eb907c
kaline <mesquitabfkaline@gmail.com>
2025-05-19 07:50:37 -0300

    Create cadastrarCidade.c

---
+++ cadastrarCidade.c
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+
+int main(void)
+{
+    struct cartaCidade{
+    
+        int estado[30];
+        int código; 
+        char nome_cidade; 
+        int população; 
+        float PIB; 
+        float area;
+        int número_pontos_turisticos;
+        //propriedade derivadas
+        float densidade_populacional;
+        float pib_per_capita;
+};
+
+struct cartaCidade cidade;
+
+printf("cadastro cidade");
+
+    return 0;
+}
